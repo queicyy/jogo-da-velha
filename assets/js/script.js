cfb05ff256acea7d51524641b0cc5d82607d6ef8
ryanitj <ryanitj@gmail.com>
2024-09-25 21:29:23 -0300

    feat: add new layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const cells = document.querySelectorAll(".cell");
 const gameBoard = document.getElementById("game-board");
 const gameStatus = document.getElementById("game-status");
+const gameTitle = document.getElementById("game-title");
+const gameForm = document.getElementById("game-form");
 const startButton = document.getElementById("start-game");
 const restartButton = document.getElementById("restart-game");
 const player1Input = document.getElementById("player1");
@@ -14,20 +16,30 @@ let player1 = "";
 let player2 = "";
 let ranking = JSON.parse(localStorage.getItem("ranking")) || [];
 
+
+displayRanking()
+fadeIn()
+
 function initGame() {
+  fadeIn()
+
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "transparent"
+  });
+
+  playerInfo.classList.add("hidden")
+
   player1 = player1Input.value || "Jogador 1";
   player2 = player2Input.value || "Jogador 2";
   currentPlayer = "O";
   board.fill("");
   cells.forEach((cell) => {
     cell.textContent = "";
-    cell.style.backgroundColor = "white";
     cell.addEventListener("click", handleCellClick, { once: true });
   });
   gameStatus.textContent = `${player1} (O) começa!`;
   gameStatus.classList.remove("hidden");
   gameBoard.classList.remove("hidden");
-  playerInfo.classList.remove("hidden");
   startButton.classList.remove("hidden");
   saveGameState();
   updateRanking();
@@ -81,9 +93,14 @@ function checkWinner() {
 
 function endGame(message) {
   gameStatus.textContent = message;
-  cells.forEach((cell) => cell.removeEventListener("click", handleCellClick));
+  cells.forEach((cell) => {
+    cell.removeEventListener("click", handleCellClick)
+  });
   updateRankingAfterGame();
   restartButton.classList.remove("hidden");
+
+  restartButton.addEventListener("click", initGame)
+
   localStorage.removeItem("board");
   localStorage.removeItem("currentPlayer");
   localStorage.removeItem("player1");
@@ -94,8 +111,7 @@ function updateRankingAfterGame() {
   const winner = currentPlayer === "0" ? player1 : player2;
 
   if (gameStatus.textContent.includes("Empate")) {
-    updatePlayerScore(player1);
-    updatePlayerScore(player2);
+    return;
   } else {
     updatePlayerScore(winner);
   }
@@ -118,7 +134,8 @@ function updatePlayerScore(player) {
 
 function displayRanking() {
   rankingList.innerHTML = "";
-  ranking.forEach((entry) => {
+
+  ranking.sort((entry1, entry2) => entry2.wins - entry1.wins ).forEach((entry) => {
     const listItem = document.createElement("li");
     listItem.textContent = `${entry.name}: ${entry.wins} vitória(s)`;
     rankingList.appendChild(listItem);
@@ -134,4 +151,19 @@ function saveGameState() {
   localStorage.setItem("player2", player2);
 }
 
-startButton.addEventListener("click", initGame);
+function fadeIn(){
+  gameTitle.classList.remove("fadeIn")
+  playerInfo.classList.remove("fadeIn")
+  gameStatus.classList.remove("fadeIn")
+
+  setTimeout(() => {
+    gameTitle.classList.add("fadeIn")
+    playerInfo.classList.add("fadeIn")
+    gameStatus.classList.add("fadeIn")
+  }, 1)
+}
+
+gameForm.addEventListener("submit", e => {
+  e.preventDefault()
+  initGame()
+})
